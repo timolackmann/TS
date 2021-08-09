9bfe51c725339e5a0da112b998d7912076c21e28
timolackmann <Timo.Lackmann@mongodb.com>
2021-08-09 14:51:11 +0200

    update

--- locustfile.py
+++ locustfile.py
@@ -1,15 +1,43 @@
-import time
-import insert_ts
+from locust import User, task, constant, tag
+from pymongo import MongoClient
+from time import perf_counter
 from datetime import datetime
-from datetime import timedelta
-from locust import HttpUser, task, between
+import greenlet
+import random
 
+class Mongo:
+    conn = ""
 
-class QuickstartUser(HttpUser):
-    wait_time = between(1, 2.5)
+    def __init__(self):
+        cs = "mongodb+srv://xxxxx"
+        self.conn = MongoClient(cs, connect=False)
+        id = ''
+    def get_conn(self):
+        return self.conn
 
+mongo_client = Mongo()
+
+class Mongouser(User):
+    def on_start(self):
+        self.id = greenlet.getcurrent().minimal_ident
     @task
-    def insert_Data(self):
-        end_date = datetime.now()
-        start_date = end_date - timedelta(days=1)
-        insert_ts.insert_data(start_date, end_date, timedelta(seconds=1))
+    @tag("INSERT")
+    def aggregate_test(self):
+        with mongo_client.get_conn() as cx:
+            db = cx.test
+
+            doc = {
+                'id': self.id,
+                'reading': 'temperature',
+                'measurement': round(random.uniform(-10, 30),4)
+            }
+            start_time = perf_counter()
+            data = db.insert_one(doc)
+            duration = perf_counter() - start_time
+
+            if data:
+                self.environment.events.request_success.fire(request_type="INSERT", name="Insert test",
+                                                             response_time=duration)
+            else:
+                self.environment.events.request_failure.fire(request_type="INSERT", name="Insert test",
+                                                             response_time=duration)
